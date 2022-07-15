6025d012cf1be8c257d9a70f3ead67860d98162e
Flavio Alfonzetti <flavioalfonzetti@gmail.com>
2022-07-15 16:50:36 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+function openNav() {
+    document.getElementById("sidebar").style.transform = "translateX(0)";
+    document.getElementById("overlay").style.opacity = "0.6";
+    document.getElementById("overlay").style.visibility = "visible"; 
+    document.getElementById("overlay").style.width = "100%";
+  }
+
+  function closeNav() {
+    document.getElementById("sidebar").style.transform = "translateX(100%)";
+    document.getElementById("overlay").style.opacity = "0";
+    document.getElementById("overlay").style.visibility = "hidden"; 
+    document.getElementById("overlay").style.width = "0";
+  }
+
+function toggleFirstAccordion() {
+    document.getElementById("accordion-1").classList.toggle("active")
+}
+function toggleSecondAccordion() {
+    document.getElementById("accordion-2").classList.toggle("active")
+}
+
+function turnArrowOne() {
+    document.querySelector(".features-mobile").classList.toggle("active")
+}
+
+function turnArrowTwo() {
+    document.querySelector(".company-mobile").classList.toggle("active")
+
+}
