63f041cb5334a097e6e7c3283497209ce253974b
Flavio Alfonzetti <flavioalfonzetti@gmail.com>
2023-05-06 12:14:58 +0200

    removed js notes

--- main.js
+++ main.js
@@ -1,31 +1,28 @@
 function openNav() {
-    document.getElementById("sidebar").style.transform = "translateX(0)";
-    document.querySelector("body").style.overflow = "hidden";
-    document.getElementById("overlay").style.opacity = "0.6";
-    document.getElementById("overlay").style.visibility = "visible"; 
-    /* document.getElementById("overlay").style.width = "100%"; */
-  }
+  document.getElementById('sidebar').style.transform = 'translateX(0)';
+  document.querySelector('body').style.overflow = 'hidden';
+  document.getElementById('overlay').style.opacity = '0.6';
+  document.getElementById('overlay').style.visibility = 'visible';
+}
 
-  function closeNav() {
-    document.getElementById("sidebar").style.transform = "translateX(100%)";
-    document.querySelector("body").style.overflow = "visible";
-    document.getElementById("overlay").style.opacity = "0";
-    document.getElementById("overlay").style.visibility = "hidden"; 
-    /* document.getElementById("overlay").style.width = "0"; */
-  }
+function closeNav() {
+  document.getElementById('sidebar').style.transform = 'translateX(100%)';
+  document.querySelector('body').style.overflow = 'visible';
+  document.getElementById('overlay').style.opacity = '0';
+  document.getElementById('overlay').style.visibility = 'hidden';
+}
 
 function toggleFirstAccordion() {
-    document.getElementById("accordion-1").classList.toggle("active")
+  document.getElementById('accordion-1').classList.toggle('active');
 }
 function toggleSecondAccordion() {
-    document.getElementById("accordion-2").classList.toggle("active")
+  document.getElementById('accordion-2').classList.toggle('active');
 }
 
 function turnArrowOne() {
-    document.querySelector(".features-mobile").classList.toggle("active")
+  document.querySelector('.features-mobile').classList.toggle('active');
 }
 
 function turnArrowTwo() {
-    document.querySelector(".company-mobile").classList.toggle("active")
-
+  document.querySelector('.company-mobile').classList.toggle('active');
 }
